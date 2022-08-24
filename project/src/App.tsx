ae929998dcc2dbd2e24a8a2d12c6b5f3bb3e6c9f
intellectualDarknet <ladyblaumeux24@gmail.com>
2022-08-24 21:48:57 +0300

    feat: download pagination

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,8 +5,8 @@ import RegistrationPage from "./pages/auth/registration/registration";
 import MainPage from "./pages/main/main";
 import Textbook from "./pages/main/components/textbook/textbook";
 import AboutUs from "./pages/main/components/about-us/about-us";
-import Home from "./pages/main/components/Home/home";
-import Promo from "./pages/main/components/Promo/promo";
+import Home from "./pages/main/components/home/home";
+import Promo from "./pages/main/components/promo/promo";
 import "./App.scss";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
           <Route path="main/*" element={<MainPage />}>
             <Route index element={<Home />} />
             <Route path="promo" element={<Promo />} />
-            <Route path="textbook" element={<Textbook />} />
+            <Route path="textbook/:group/:page" element={<Textbook />} />
             <Route path="about-us" element={<AboutUs />} />
           </Route>
           <Route path="*" element={<Navigate to="main" />} />
